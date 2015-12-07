69143d42b0cee5f10c90eb2024add2c784e71da3
Sirijet Sims <sirijet.s@gmail.com>
2015-12-07 22:00:06 +0700

    Add mx32bit module

---
+++ mux32.v
@@ -0,0 +1,15 @@
+module mux32 (
+	output reg  [31:0] out,
+	input [31:0] in0,
+	input [31:0] in1,
+	input sel 
+	);
+
+  	always @(sel)
+    begin
+      if (sel == 0)
+            out = in0;
+      else
+            out = in1;
+    end
+endmodule
