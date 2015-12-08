1516d95a2c3d1fee4411afd3cbc66d9d11f522ad
Sirijet Sims <sirijet.s@gmail.com>
2015-12-08 23:37:38 +0700

    Add testbench for control unit

---
+++ control_unit/tb_control_unit.v
@@ -0,0 +1,18 @@
+module tb_control_unit;
+  	reg [5:0] op;
+  	wire regdst, regwrite;
+	wire branch;
+	wire jump;
+	wire memread, memtoreg, memwrite;
+  	wire [1:0] aluop;
+	wire aluscr;
+
+  control_unit cu1 (op, regdst, regwrite, branch, jump, memread, memtoreg, memwrite, aluop, aluscr);
+
+  initial
+    begin
+      op <= 5'b00000;
+      $dumpfile("dump.vcd");
+      $dumpvars(1);
+    end
+endmodule;
