75d3ba644f091d7f3fa00c813564d6314787599c
Sirijet Sims <sirijet.s@gmail.com>
2015-12-10 03:58:05 +0700

    Fix bugs

--- cpu.v
+++ cpu.v
@@ -13,7 +13,6 @@
 module cpu (
   	  input clk,
   	  output reg [31:0] addr,
-  	  output reg [31:0] pc,
 	  output [31:0] instruction,
 	  output [1:0] cu_regdst,
 	  output cu_jump, cu_branch, cu_memread,
@@ -30,57 +29,41 @@ module cpu (
 	  output [3:0] aluctrl_out,
 	  output [31:0] dmem_readdata,
 	  output bBranch,
-      output [31:0] j_addr
+      output [31:0] j_addr,
+      output [31:0] next_pc
 	  );
-  		//reg[31:0] pc;
-  		initial begin
-          addr = 32'b0000_0000_0000_0000_0000_0000_0010_1000;
-        end
-
-
-  		reg [31:0] ra;
-  		parameter ra_addr = 5'b11111;
-
-	    instr_mem instrmem (addr,instruction);
-	    control_unit contrlu (instruction[31:26],
-                              cu_regdst, cu_regwrite, cu_branch,
-	                          cu_jump, cu_memread, cu_memtoreg,
-	                          cu_memwrite, cu_aluop, cu_aluscr );
-  		mux5_3to1 mux01 (mux1_regwrite, instruction[20:16], instruction[15:11], ra_addr, cu_regdst[1:0]);
-	  	registerMemory regmem ( .reg_read1(instruction[25:21]),
-								.reg_read2(instruction[20:16]),
-	                           	.reg_write(mux1_regwrite[4:0]),
-	                          	.regwrite_con(cu_regwrite),
-	                          	.write_data(mux3_writedata),
-	                          	.data1(reg_readdata1),
-	                          	.data2(reg_readdata2));
-	  	sign_extended signext (signext_out[31:0], instruction[15:0]);
-	  	mux32_2to1 mux02 (mux2_out, reg_readdata2, signext_out, cu_aluscr);
-	    alu_control aluctrl (cu_aluop, instruction[5:0], aluctrl_out);
-	    alu ALU (aluctrl_out, reg_readdata1, mux2_out, alu_out, alu_zero);
-	    datamem data_mem (dmem_readdata, alu_out, reg_readdata2, cu_memread, cu_memwrite);
-
-    always @(posedge clk) begin
-       pc <= addr + 4;
-
+    wire reg[31:0] pc;
+    initial begin
+        addr = 32'b0000_0000_0000_0000_0000_0000_0010_1000;
     end
-  		//assign addr = pc;
-  		mux32_3to1 mux03(mux3_writedata, alu_out, dmem_readdata, pc, cu_memtoreg);
 
-	    and AND1(bBranch, cu_branch, alu_zero );
+    reg [31:0] ra;
+    parameter ra_addr = 5'b11111;
 
-	  	wire [31:0] branch_shiftl2_result;
-	  	assign branch_shiftl2_result = signext_out << 2;
+    instr_mem instrmem (addr,instruction);
+    assign pc = addr + 4;
+    control_unit contrlu (instruction[31:26], cu_regdst, cu_regwrite, cu_branch, cu_jump, cu_memread, cu_memtoreg, cu_memwrite, cu_aluop, cu_aluscr);
+    mux5_3to1 mux01 (mux1_regwrite, instruction[20:16], instruction[15:11], ra_addr, cu_regdst[1:0]);
+    registerMemory regmem (instruction[25:21], instruction[20:16], mux1_regwrite[4:0], cu_regwrite, mux3_writedata, reg_readdata1, reg_readdata2);
+    sign_extended signext (signext_out[31:0], instruction[15:0]);
+    mux32_2to1 mux02 (mux2_out, reg_readdata2, signext_out, cu_aluscr);
+    alu_control aluctrl (cu_aluop, instruction[5:0], aluctrl_out);
+    alu ALU (aluctrl_out, reg_readdata1, mux2_out, alu_out, alu_zero);
+    datamem data_mem (dmem_readdata, alu_out, reg_readdata2, cu_memread, cu_memwrite);
+    mux32_3to1 mux03(mux3_writedata, alu_out, dmem_readdata, pc, cu_memtoreg);
 
+    always @(posedge clk) addr <= next_pc;
 
-	  	assign j_addr = {pc[31:28],(instruction[25:0] << 2)};
+    and AND1(bBranch, cu_branch, alu_zero );
 
-	  wire [31:0] b_addr;
-	  assign b_addr = pc + branch_shiftl2_result;
-	  wire [31:0] mux04_result;
-      wire [31:0] mux05_result;
+    wire [31:0] branch_shiftl2_result;
+    wire [31:0] b_addr;
+    wire [31:0] mux04_result;
+    assign branch_shiftl2_result = signext_out << 2;
+    assign b_addr = pc + branch_shiftl2_result;
+    assign j_addr = {pc[31:28],(instruction[25:0] << 2)};
 
-	  mux32_2to1 mux04 (mux04_result, pc, b_addr, bBranch);
-	  mux32_2to1 mux05 (mux05_result, mux04_result, j_addr, cu_jump);
-      
+    mux32_2to1 mux04 (mux04_result, pc, b_addr, bBranch);
+    mux32_2to1 mux05 (next_pc, mux04_result, j_addr, cu_jump);
+
 endmodule
--- tb_cpu.v
+++ tb_cpu.v
@@ -2,10 +2,7 @@
 // or browse Examples
 module tb_cpu;
     reg clk;
-    //reg [31:0] addr;
   	wire [31:0] addr;
-    wire [31:0] pcp4;
-    //wire [31:0] out;
   	wire[31:0] instruction;
     wire[1:0] cu_regdst;
     wire cu_jump, cu_branch, cu_memread;
@@ -23,9 +20,9 @@ module tb_cpu;
     wire[31:0] dmem_readdata;
   	wire bBranch;
     wire [31:0] j_addr;
+    wire [31:0] next_pc;
 
-    cpu CPU1(clk, addr, pcp4, instruction, cu_regdst, cu_jump, cu_branch, cu_memread, cu_memtoreg, cu_aluop, cu_memwrite, cu_aluscr, cu_regwrite, mux1_regwrite, mux3_writedata, reg_readdata1, reg_readdata2, signext_out, mux2_out, alu_out, alu_zero, aluctrl_out, dmem_readdata, bBranch, j_addr);
-
+    cpu CPU1(clk, addr, instruction, cu_regdst, cu_jump, cu_branch, cu_memread, cu_memtoreg, cu_aluop, cu_memwrite, cu_aluscr, cu_regwrite, mux1_regwrite, mux3_writedata, reg_readdata1, reg_readdata2, signext_out, mux2_out, alu_out, alu_zero, aluctrl_out, dmem_readdata, bBranch, j_addr, next_pc);
 
   initial begin
     $dumpfile("dump.vcd");
