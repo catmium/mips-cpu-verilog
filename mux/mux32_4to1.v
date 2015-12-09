8ff39fd941c616849ca6e41ed6a6dd351dd286bc
Sirijet Sims <sirijet.s@gmail.com>
2015-12-09 22:11:49 +0700

    Add 4to1 mux

---
+++ mux/mux32_4to1.v
@@ -0,0 +1,18 @@
+module mux32_4to1 (out, in0, in1, in2, in3, sel);
+	output reg  [4:0] out;
+	input [31:0] in0;
+	input [31:0] in1;
+	input [31:0] in2;
+	input [31:0] in3;
+	input [3:0] sel;
+
+  	always @(sel)
+    begin
+		case (sel)
+			2'b00: out = in0;
+			2'b01: out = in1;
+			2'b10: out = in2;
+			2'b11: out = in3;
+		endcase
+    end
+endmodule
