31a3132c2c4de295a0d97a39d231f916f20a968e
Sirijet Sims <sirijet.s@gmail.com>
2015-12-10 04:24:18 +0700

    Add more instructions

--- instruction_mem/instr_mem.v
+++ instruction_mem/instr_mem.v
@@ -8,7 +8,9 @@ module instr_mem (	input [31:0] address,
 		begin
 			for (i=0; i<250; i=i+1) memory[i] = 32'b0;
 			// Insert MIPS's assembly here start at memory[10] = ...
-			memory[10] = 32'b001000_00000_01000_00000_00000_000101;    // addi $t0, $0, 5
+			memory[10] = 32'b001000_00000_01000_0000000000000010; //addi $t0, $0, 2
+			memory[11] = 32'b101011_00100_00101_00000_00000_000000;    //110100 sw
+            memory[12] = 32'b100011_00100_00110_00000_00000_000000; 	// 111000 lw
 		end
 
 	assign instruction = memory[address >> 2];
